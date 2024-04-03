f76792aa5baf2d78575eac349f47e9a48b8fb98a
Ben <ben.blankenburg@icloud.com>
2024-04-03 13:19:33 +0000

    added archetecture check

--- k230-sys/build.rs
+++ k230-sys/build.rs
@@ -8,6 +8,7 @@ fn main() {
 
     
 }
+#[cfg(not(target_arch = "riscv64"))]
 fn main() {
     panic!("This crate should not be built for the host architecture {}",  env::current_dir().unwrap().to_str().unwrap())
 }
