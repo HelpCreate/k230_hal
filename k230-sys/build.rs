e91b9870b7a56fe2139f698bbe701794e5d725f1
Ben <ben.blankenburg@icloud.com>
2024-04-03 14:27:58 +0000

    basic target

--- k230-sys/build.rs
+++ k230-sys/build.rs
@@ -1,15 +1,16 @@
-use std::{fs, env};
+use build_target;
+use std::env;
 
-
-#[cfg(target_arch = "riscv64")]
 fn main() {
-    
+    let target = build_target::target_triple().unwrap();
 
-
-    
+    enum TARGET {
+        LittleCore,
+        BigCore,
+    }
+    let target = match target.as_str() {
+        "riscv32imac-unknown-none-elf" => TARGET::LittleCore,
+        "riscv64imac-unknown-none-elf" => TARGET::BigCore,
+        _ => panic!("This crate should not be built for the host architecture"),
+    };
 }
-#[cfg(not(target_arch = "riscv64"))]
-fn main() {
-    panic!("This crate should not be built for the host architecture {}",  env::current_dir().unwrap().to_str().unwrap())
-}
-
