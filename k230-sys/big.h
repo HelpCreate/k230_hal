ef8219434533f201438db6e81113c3d5764b255a
Ben <ben.blankenburg@icloud.com>
2024-05-23 19:45:32 +0000

    first only ipcmsg

--- k230-sys/big.h
+++
@@ -1,1 +0,0 @@
-#include "k230_sdk/src/big/rt-smart/kernel/bsp/maix3/board/interdrv/gpio/drv_gpio.h"
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,2 @@
-// pub mod ipcmsg;
-pub mod big;
-pub mod small;
+pub mod ipcmsg;
 
-enum CORE {
-    Little,
-    Big,
-}
-const WHICH_CORE: CORE = if cfg!(musl) { CORE::Big } else { CORE::Little };
